605f6e774d4842f6d0563546a16395ff5144335d
omer kor <omerkorr@gmail.com>
2020-02-12 17:55:39 +0300

    last

--- ToplamaOgret/ToplamaOgret/Program.cs
+++ ToplamaOgret/ToplamaOgret/Program.cs
@@ -10,40 +10,48 @@ namespace ToplamaOgret
     {
         static void Main(string[] args)
         {
-            int puan = 0 ;
-            BASADON:
+        BASADON:
+            int puan = 0;
             Random rand = new Random();
-            int sayi1 = rand.Next(1, 100);
-            int sayi2 = rand.Next(1, 100);
-            Console.WriteLine("Aşağıdaki işlemin sonucunu yazınız.");
-            Console.WriteLine("{0}+{1}=", sayi1, sayi2);
-            int sonuc = Convert.ToInt32(Console.ReadLine());
-            if (puan >= 0 && puan < 100)
+            int sonuc;
+            do
             {
+                int sayi1 = rand.Next(1, 100);
+                int sayi2 = rand.Next(1, 100);
+                Console.WriteLine("Aşağıdaki işlemin sonucunu yazınız.");
+                Console.WriteLine("{0}+{1}=", sayi1, sayi2);
+                sonuc = Convert.ToInt32(Console.ReadLine());
                 if (sonuc == (sayi1 + sayi2))
                 {
                     puan = puan + 20;
                     Console.WriteLine("Tebrikler doğru cevap 20 puan kazandınız \nToplam Puanınız {0}", puan);
-                    goto BASADON;
                 }
                 else
                 {
                     puan = puan - 10;
-                    Console.WriteLine("Üzgünüz yanlış cevaplandı 10 puan kaybettiniz \n Toplam Puanınız {0}", puan);
-                    goto BASADON;
+                    Console.WriteLine("Üzgünüz yanlış cevaplandı 10 puan kaybettiniz \nToplam Puanınız {0}", puan);
                 }
-            }
-            else if (puan < 0)
+            } while (puan < 100 && puan >= 0);
+            if (puan==100)
             {
-                Console.WriteLine("oyunu kaybettiniz... :(");
+                Console.WriteLine("Tebrikler oyunu kazandınız");
             }
-            else if (puan == 100)
+            if (puan < 0)
             {
-                Console.WriteLine("Oyunu kazandınız... :)");
+                Console.WriteLine("Üzgünüz kaybettiniz");
             }
-
-
-            Console.Read();
+            Console.WriteLine("Yeni oyun için 1 e basınız.");
+            char secim = Convert.ToChar(Console.ReadLine());
+            if (secim=='1')
+            {
+                goto BASADON;
+            }
+            else
+            {
+                Console.WriteLine("Güle Güle....");
+            }
+            Console.ReadKey();
+            
         }
     }
 }
