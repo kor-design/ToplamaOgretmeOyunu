c67780ed5c0b8eeafa49742873dd10c6db79c3db
omer kor <omerkorr@gmail.com>
2020-02-12 17:34:42 +0300

    last

--- ToplamaOgret/ToplamaOgret/Program.cs
+++ ToplamaOgret/ToplamaOgret/Program.cs
@@ -10,6 +10,40 @@ namespace ToplamaOgret
     {
         static void Main(string[] args)
         {
+            int puan = 0 ;
+            BASADON:
+            Random rand = new Random();
+            int sayi1 = rand.Next(1, 100);
+            int sayi2 = rand.Next(1, 100);
+            Console.WriteLine("Aşağıdaki işlemin sonucunu yazınız.");
+            Console.WriteLine("{0}+{1}=", sayi1, sayi2);
+            int sonuc = Convert.ToInt32(Console.ReadLine());
+            if (puan >= 0 && puan < 100)
+            {
+                if (sonuc == (sayi1 + sayi2))
+                {
+                    puan = puan + 20;
+                    Console.WriteLine("Tebrikler doğru cevap 20 puan kazandınız \nToplam Puanınız {0}", puan);
+                    goto BASADON;
+                }
+                else
+                {
+                    puan = puan - 10;
+                    Console.WriteLine("Üzgünüz yanlış cevaplandı 10 puan kaybettiniz \n Toplam Puanınız {0}", puan);
+                    goto BASADON;
+                }
+            }
+            else if (puan < 0)
+            {
+                Console.WriteLine("oyunu kaybettiniz... :(");
+            }
+            else if (puan == 100)
+            {
+                Console.WriteLine("Oyunu kazandınız... :)");
+            }
+
+
+            Console.Read();
         }
     }
 }
